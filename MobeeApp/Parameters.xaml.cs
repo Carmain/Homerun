55a7cc1de0ebcd727eab441c8129ef572bcd7b4d
Billard Martin <martin.l.billard@gmail.com>
2015-06-06 22:21:22 +0200

    Get a contact from the repertoire

--- MobeeApp/Parameters.xaml.cs
+++ MobeeApp/Parameters.xaml.cs
@@ -7,19 +7,42 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
 
 namespace MobeeApp
 {
     public partial class Parameters : PhoneApplicationPage
     {
+        private PhoneNumberChooserTask phoneNumberChooserTask;
         public Parameters()
         {
             InitializeComponent();
+            phoneNumberChooserTask = new PhoneNumberChooserTask();
+            phoneNumberChooserTask.Completed += new EventHandler<PhoneNumberResult>(phoneNumberChooserTask_Completed);
         }
 
         private void Back_main_page(object sender, System.Windows.Input.GestureEventArgs e)
         {
             NavigationService.GoBack();
+        }
+
+        private void Choose_contact(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            phoneNumberChooserTask.Show();
+        }
+
+        void phoneNumberChooserTask_Completed(object sender, PhoneNumberResult e)
+        {
+            if (e.TaskResult == TaskResult.OK)
+            {
+                MessageBox.Show("The phone number for " + e.DisplayName + " is " + e.PhoneNumber);
+
+                //Code to start a new call using the retrieved phone number.
+                //PhoneCallTask phoneCallTask = new PhoneCallTask();
+                //phoneCallTask.DisplayName = e.DisplayName;
+                //phoneCallTask.PhoneNumber = e.PhoneNumber;
+                //phoneCallTask.Show();
+            }
         }
     }
 }
