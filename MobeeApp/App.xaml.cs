cbfc38d09c4f39567f4a713c9c926fd121a89bce
Billard Martin <martin.l.billard@gmail.com>
2015-06-09 16:16:06 +0200

    Remove useless comments

--- MobeeApp/App.xaml.cs
+++ MobeeApp/App.xaml.cs
@@ -38,20 +38,7 @@ namespace MobeeApp
             // Affichez des informations de profilage graphique lors du débogage.
             if (Debugger.IsAttached)
             {
-                // Affichez les compteurs de fréquence des trames actuels.
                 Application.Current.Host.Settings.EnableFrameRateCounter = true;
-
-                // Affichez les zones de l'application qui sont redessinées dans chaque frame.
-                //Application.Current.Host.Settings.EnableRedrawRegions = true;
-
-                // Activez le mode de visualisation d'analyse hors production,
-                // qui montre les zones d'une page sur lesquelles une accélération GPU est produite avec une superposition colorée.
-                //Application.Current.Host.Settings.EnableCacheVisualization = true;
-
-                // Empêche l'écran de s'éteindre lorsque le débogueur est utilisé en désactivant
-                // la détection de l'état inactif de l'application.
-                // Attention :- À utiliser uniquement en mode de débogage. Les applications qui désactivent la détection d'inactivité de l'utilisateur continueront de s'exécuter
-                // et seront alimentées par la batterie lorsque l'utilisateur ne se sert pas du téléphone.
                 PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Disabled;
             }
 
@@ -164,52 +151,16 @@ namespace MobeeApp
 
         #endregion
 
-        // Initialise la police de l'application et le sens du flux tels qu'ils sont définis dans ses chaînes de ressource localisées.
-        //
-        // Pour vous assurer que la police de votre application est alignée avec les langues prises en charge et que le
-        // FlowDirection pour chacune de ces langues respecte le sens habituel, ResourceLanguage
-        // et ResourceFlowDirection doivent être initialisés dans chaque fichier resx pour faire correspondre ces valeurs avec la
-        // culture du fichier. Par exemple :
-        //
-        // AppResources.es-ES.resx
-        //    La valeur de ResourceLanguage doit être « es-ES »
-        //    La valeur de ResourceFlowDirection doit être « LeftToRight »
-        //
-        // AppResources.ar-SA.resx
-        //     La valeur de ResourceLanguage doit être « ar-SA »
-        //     La valeur de ResourceFlowDirection doit être « RightToLeft »
-        //
-        // Pour plus d'informations sur la localisation des applications Windows Phone, consultez le site http://go.microsoft.com/fwlink/?LinkId=262072.
-        //
         private void InitializeLanguage()
         {
             try
             {
-                // Définissez la police pour qu'elle corresponde à la langue d'affichage définie par la
-                // chaîne de ressource ResourceLanguage pour chaque langue prise en charge.
-                //
-                // Rétablit la police de la langue neutre si la langue d'affichage
-                // du téléphone n'est pas prise en charge.
-                //
-                // Si une erreur de compilateur est détectée, ResourceLanguage est manquant dans
-                // le fichier de ressources.
                 RootFrame.Language = XmlLanguage.GetLanguage(AppResources.ResourceLanguage);
-
-                // Définit FlowDirection pour tous les éléments sous le frame racine en fonction de la
-                // de la chaîne de ressource ResourceFlowDirection pour chaque
-                // langue prise en charge.
-                //
-                // Si une erreur de compilateur est détectée, ResourceFlowDirection est manquant dans
-                // le fichier de ressources.
                 FlowDirection flow = (FlowDirection)Enum.Parse(typeof(FlowDirection), AppResources.ResourceFlowDirection);
                 RootFrame.FlowDirection = flow;
             }
             catch
             {
-                // Si une exception est détectée ici, elle est probablement due au fait que
-                // ResourceLanguage n'est pas correctement défini sur un code de langue pris en charge
-                // ou que ResourceFlowDirection est défini sur une valeur différente de LeftToRight
-                // ou RightToLeft.
 
                 if (Debugger.IsAttached)
                 {
