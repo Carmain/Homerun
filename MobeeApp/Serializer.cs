b345e113faef87a7f15e1528a2303f97411da373
Billard Martin <martin.l.billard@gmail.com>
2015-06-10 12:58:04 +0200

    Creating a class for serialization

---
+++ MobeeApp/Serializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MobeeApp
+{
+    public class Serializer
+    {
+        public Serializer() { }
+
+        public string serialize(object objectToSerialize) {
+            XmlSerializer xml = new XmlSerializer(objectToSerialize.GetType());
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xml.Serialize(textWriter, objectToSerialize);
+                return textWriter.ToString();
+            }
+        }
+
+        public object deserialize(string stringToDeserialize, Type type)
+        {
+            var serializer = new XmlSerializer(type);
+            using (TextReader reader = new StringReader(stringToDeserialize))
+            {
+                return serializer.Deserialize(reader);
+            }
+        }
+    }
+}
